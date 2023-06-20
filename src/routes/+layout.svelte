eafc3524cfb10057dab4911e75acee04737b7522
Dmytro Cheglakov <dcheglakov@gmail.com>
2023-06-20 23:14:34 +0300

    panda css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { stack } from 'styled-system/patterns';
+</script>
+
+<main class={stack({ gap: '4' })}>
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { css } from 'styled-system/css';
+</script>
+
+<h1 class={css({ fontSize: '4xl', fontWeight: 'bold' })}>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
